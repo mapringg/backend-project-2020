6a6c5952665e72b5df37423701b130f60944c790
Brandon <brandonfox@hotmail.co.uk>
2020-03-10 19:45:01 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# backend-project-2020
---
+++ theia-controller/src/main/kotlin/backend/project/theiacontroller/TheiaControllerApplication.kt
@@ -0,0 +1,11 @@
+package backend.project.theiacontroller
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TheiaControllerApplication
+
+fun main(args: Array<String>) {
+	runApplication<TheiaControllerApplication>(*args)
+}
